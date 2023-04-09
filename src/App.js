f901685ca528e9956a4d6a6e03f582c1b16eab7a
ndurgesh88 <durgeshnidadavolu@gmail.com>
2023-04-09 13:03:49 +0800

    Add files from 7.signup-form project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="intro-herounit">
+      <div className="intro-herounit__content">
+        <h1 className="intro-herounit__content__heading">Learn to code by watching others</h1>
+        <p className="intro-herounit__content__paragraph">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
