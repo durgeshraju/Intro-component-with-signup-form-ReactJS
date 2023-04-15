c301bd654376c81ea70be2c4680661277ba200e2
ndurgesh88 <durgeshnidadavolu@gmail.com>
2023-04-15 16:40:16 +0800

    Update Input withb props react icons

--- src/ComponentUILibrary/Inputs.js
+++ src/ComponentUILibrary/Inputs.js
@@ -1,9 +1,12 @@
+import { FaExclamationCircle } from 'react-icons/fa';
+
 const Inputs = ({htmlFor, label, name, id, placeholder, type, error}) => {
     return (
     <div className="signup-form__input-container">
         <label htmlFor={htmlFor} className="signup-form__label">{label}</label>
-        <input type={type} name={name} id={id} placeholder={placeholder} className="signup-form__input"/>
-        {error && <span className="signup-form__error">{error}</span>}
+        <input type={type} name={name} id={id} placeholder={placeholder} 
+        className={`signup-form__input ${error && 'signup-form__input--error'}`}/>
+        {error && <span className="signup-form__error"><FaExclamationCircle size={24} className="signup-form__error--icon" />{error}</span>}
     </div>
     )
 }
