ad0bdfac8eeab348e6d9f81a75e2299a0a1b25a4
ndurgesh88 <durgeshnidadavolu@gmail.com>
2023-04-15 16:39:32 +0800

    added few more props..

--- src/ComponentUILibrary/Button.js
+++ src/ComponentUILibrary/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Button = ({ boldText, normalText, color, type, className, onClick }) => {
+const Button = ({ boldText, normalText, color, type, className, onClick, fontSize}) => {
   const classes = classNames('button', {
     'button--primary': color === 'primary',
     'button--secondary': color === 'secondary',
@@ -9,7 +9,7 @@ const Button = ({ boldText, normalText, color, type, className, onClick }) => {
   });
 
   return (
-    <button type={type} className={classes} onClick={onClick}>
+    <button type={type} className={classes} onClick={onClick} style={{fontSize}}>
       <span><strong>{boldText}</strong> <span>{normalText}</span></span>
     </button>
   );
